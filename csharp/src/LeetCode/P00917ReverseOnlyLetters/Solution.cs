e95595a1fd16eebe762150709d013c0fffbaa957
Tesmont <ivantsovartem@outlook.com>
2026-05-02 21:44:23 +0200

    917

--- csharp/src/LeetCode/P00917ReverseOnlyLetters/Solution.cs
+++ csharp/src/LeetCode/P00917ReverseOnlyLetters/Solution.cs
@@ -8,6 +8,35 @@ public sealed class Solution
 {
     public string ReverseOnlyLetters(string s)
     {
-        throw new System.NotImplementedException();
+        int leftIndex = 0;
+        int rightIndex = s.Length - 1;
+        char[] result = new char[s.Length];
+
+        while (leftIndex <= rightIndex)
+        {
+            char leftChar = s[leftIndex];
+            if (!char.IsAsciiLetter(leftChar))
+            {
+                result[leftIndex] = leftChar;
+                leftIndex++;
+                continue;
+            }
+
+            char rightChar = s[rightIndex];
+            if (!char.IsAsciiLetter(rightChar))
+            {
+                result[rightIndex] = rightChar;
+                rightIndex--;
+                continue;
+            }
+
+            result[leftIndex] = rightChar;
+            result[rightIndex] = leftChar;
+
+            leftIndex++;
+            rightIndex--;
+        }
+
+        return new string(result);
     }
 }
