8d1bfe45281a17878fdbf208ba0d349a209e54c8
Tesmont <ivantsovartem@outlook.com>
2026-05-08 14:06:56 +0200

    LeetCode 49. Group Anagrams.

---
+++ csharp/tests/LeetCode.Tests/P00049GroupAnagrams/SolutionTests.cs
@@ -0,0 +1,35 @@
+using LeetCode.P00049GroupAnagrams;
+using Xunit;
+
+namespace LeetCode.Tests.P00049GroupAnagrams;
+
+public sealed class SolutionTests
+{
+    public static TheoryData<string[], string[][]> GroupAnagramsCases =>
+        new()
+        {
+            { ["eat", "tea", "tan", "ate", "nat", "bat"], [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]] },
+            { [""], [[""]] },
+            { ["a"], [["a"]] },
+        };
+
+    [Theory]
+    [MemberData(nameof(GroupAnagramsCases))]
+    public void GroupAnagrams_ReturnsGroupedAnagrams(string[] strs, string[][] expected)
+    {
+        Solution solution = new();
+
+        IList<IList<string>> actual = solution.GroupAnagrams(strs);
+
+        Assert.Equal(Normalize(expected), Normalize(actual));
+    }
+
+    private static string[][] Normalize(IEnumerable<IEnumerable<string>> groups)
+    {
+        return groups
+            .Select(group => group.Order().ToArray())
+            .OrderBy(group => group[0])
+            .ThenBy(group => group.Length)
+            .ToArray();
+    }
+}
