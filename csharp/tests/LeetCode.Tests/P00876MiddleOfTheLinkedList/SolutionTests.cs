67809238264edbdc8e197eb29719927c74a4db02
Tesmont <ivantsovartem@outlook.com>
2026-05-20 22:44:32 +0200

    LeetCode #876 - Middle of the Linked List.

---
+++ csharp/tests/LeetCode.Tests/P00876MiddleOfTheLinkedList/SolutionTests.cs
@@ -0,0 +1,47 @@
+using LeetCode.P00876MiddleOfTheLinkedList;
+using Xunit;
+
+namespace LeetCode.Tests.P00876MiddleOfTheLinkedList;
+
+public sealed class SolutionTests
+{
+    [Theory]
+    [InlineData(new[] { 1, 2, 3, 4, 5 }, 2)]
+    [InlineData(new[] { 1, 2, 3, 4, 5, 6 }, 3)]
+    public void MiddleNode_ReturnsMiddleNode(int[] values, int expectedIndex)
+    {
+        Solution solution = new();
+        ListNode head = BuildList(values);
+        ListNode expected = GetNodeAt(head, expectedIndex);
+
+        ListNode actual = solution.MiddleNode(head);
+
+        Assert.Same(expected, actual);
+    }
+
+    private static ListNode BuildList(int[] values)
+    {
+        ListNode head = new(values[0]);
+        ListNode current = head;
+
+        for (int i = 1; i < values.Length; i++)
+        {
+            current.next = new ListNode(values[i]);
+            current = current.next;
+        }
+
+        return head;
+    }
+
+    private static ListNode GetNodeAt(ListNode head, int index)
+    {
+        ListNode current = head;
+
+        for (int i = 0; i < index; i++)
+        {
+            current = current.next!;
+        }
+
+        return current;
+    }
+}
