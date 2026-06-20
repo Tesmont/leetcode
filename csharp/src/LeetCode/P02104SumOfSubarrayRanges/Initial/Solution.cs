95bb93dacafdf8dba1543d649b471e49bd9f8c1c
Tesmont <ivantsovartem@outlook.com>
2026-06-20 10:05:10 +0200

    LeetCode 2104. Sum of Subarray Ranges.

--- csharp/src/LeetCode/P02104SumOfSubarrayRanges/Initial/Solution.cs
+++ csharp/src/LeetCode/P02104SumOfSubarrayRanges/Initial/Solution.cs
@@ -4,10 +4,79 @@ namespace LeetCode.P02104SumOfSubarrayRanges.Initial;
 /// LeetCode 2104. Sum of Subarray Ranges.
 /// https://leetcode.com/problems/sum-of-subarray-ranges/
 /// </summary>
-public sealed class Solution
+public class Solution
 {
     public long SubArrayRanges(int[] nums)
     {
-        throw new System.NotImplementedException();
+        long sumMinimums = SumMinimums(nums);
+        long sumMaximums = SumMaximums(nums);
+
+        return sumMaximums - sumMinimums;
+    }
+
+    private static long SumMinimums(int[] nums)
+    {
+        Stack<int> stack = new(nums.Length);
+        long result = 0;
+
+        for (int i = 0; i <= nums.Length; i++)
+        {
+            while (stack.Count > 0 &&
+                   (i == nums.Length || nums[i] <= nums[stack.Peek()]))
+            {
+                int mid = stack.Pop();
+
+                int leftBoundary = stack.Count == 0
+                    ? -1
+                    : stack.Peek();
+
+                int rightBoundary = i;
+
+                long left = mid - leftBoundary;
+                long right = rightBoundary - mid;
+
+                result += nums[mid] * left * right;
+            }
+
+            if (i < nums.Length)
+            {
+                stack.Push(i);
+            }
+        }
+
+        return result;
+    }
+
+    private static long SumMaximums(int[] nums)
+    {
+        Stack<int> stack = new(nums.Length);
+        long result = 0;
+
+        for (int i = 0; i <= nums.Length; i++)
+        {
+            while (stack.Count > 0 &&
+                   (i == nums.Length || nums[i] >= nums[stack.Peek()]))
+            {
+                int mid = stack.Pop();
+
+                int leftBoundary = stack.Count == 0
+                    ? -1
+                    : stack.Peek();
+
+                int rightBoundary = i;
+
+                long left = mid - leftBoundary;
+                long right = rightBoundary - mid;
+
+                result += nums[mid] * left * right;
+            }
+
+            if (i < nums.Length)
+            {
+                stack.Push(i);
+            }
+        }
+
+        return result;
     }
 }
